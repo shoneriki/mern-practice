4cc86c703b818175d81d2dbf6660ddb680086772
Sho Neriki <ikirenohs@gmail.com>
2023-06-13 16:36:24 +0900

    connected to mongodb, gitignore

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,21 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import "dotenv/config.js";
+
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
 
 app.listen(3001, () => console.log("Server Started"))
