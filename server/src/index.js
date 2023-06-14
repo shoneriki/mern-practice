fed88f44d58582ebede58721922dad0a49dbad9a
Sho Neriki <ikirenohs@gmail.com>
2023-06-14 10:49:16 +0900

    established connection with api for register

--- server/src/index.js
+++ server/src/index.js
@@ -2,13 +2,14 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import "dotenv/config.js";
+import {userRouter} from './routes/user.js'
 
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-
+app.use("/auth", userRouter)
 
 
 mongoose.connect(process.env.MONGO_URI, {
