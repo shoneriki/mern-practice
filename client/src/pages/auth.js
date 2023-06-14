bdf3bcb57cc4f80d5295a6d316a7cae1349a36ac
Sho Neriki <ikirenohs@gmail.com>
2023-06-14 16:04:58 +0900

    changed forms in auth.js as well

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -3,6 +3,8 @@ import axios from "axios";
 import {useCookies} from "react-cookie";
 import {useNavigate} from "react-router-dom";
 
+import {AuthForm} from "../components/AuthForm"
+
 export const Auth = () => {
   return (
     <div className="auth">
@@ -22,44 +24,30 @@ const Register = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      await axios.post("http://localhost:3001/auth/register", {
+      const result = await axios.post("http://localhost:3001/auth/register", {
         username,
         password,
       });
-      alert("Registration Completed!")
+      alert("Registration Completed!");
+      setCookies("access_token", result.data.token);
+      window.localStorage.setItem("userID", result.data.userID);
       navigate("/");
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <h2>Register</h2>
-        <div className="form-group">
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </div>
-        <button type="submit">Register</button>
-      </form>
-    </div>
+    <AuthForm
+      username={username}
+      setUsername={setUsername}
+      password={password}
+      setPassword={setPassword}
+      handleSubmit={handleSubmit}
+      authText="Register"
+    />
   );
-}
+};
 
 
 const Login = () => {
@@ -79,38 +67,26 @@ const Login = () => {
         password,
       });
 
-      setCookies("access_token", result.data.token);
-      window.localStorage.setItem("userID", result.data.userID);
-      navigate("/");
+      if (result.data.message) {
+        alert(result.data.message);
+      } else {
+        setCookies("access_token", result.data.token);
+        window.localStorage.setItem("userID", result.data.userID);
+        navigate("/");
+      }
     } catch (error) {
       console.error(error);
     }
   };
 
   return (
-    <div className="auth-container">
-      <form onSubmit={handleSubmit}>
-        <h2>Login</h2>
-        <div className="form-group">
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <AuthForm
+      username={username}
+      setUsername={setUsername}
+      password={password}
+      setPassword={setPassword}
+      handleSubmit={handleSubmit}
+      authText="Login"
+    />
   );
 };
