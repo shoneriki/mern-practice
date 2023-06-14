e53195bf07f28c3cceb64e340c2280041bf02b7e
Sho Neriki <ikirenohs@gmail.com>
2023-06-14 12:52:10 +0900

    login endpoint

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
+import "dotenv/config.js";
 import {UserModel} from '../models/Users.js'
 
 const router = express.Router()
@@ -9,9 +10,34 @@ router.post("/register", async (req, res) => {
   const {username, password} = req.body;
   const user = await UserModel.findOne({username});
 
-  res.json(user);
+  if(user) {
+    return res.json({message: "User already exists!"});
+  }
+
+  const hashedPassword = await bcrypt.hash(password,10);
+
+  const newUser = new UserModel({username, password: hashedPassword});
+  await newUser.save()
+
+  res.json({message: "User Registered Successfully"});
 })
 
-// router.post("/login")
+router.post("/login", async (req, res) => {
+  const {username, password} = req.body;
+  const user = await UserModel.findOne({username})
+
+  if(!user) {
+    return res.json({message: "User Doesn't Exist..."})
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if(!isPasswordValid) {
+    return res.json({message: "username or password is incorrect"})
+  }
+
+  const token = jwt.sign({id: user._id}, process.env.SECRET)
+  res.json({token, userID: user._id})
+})
 
 export {router as userRouter}
