ed52e7ffbca372274cf817815a1527c0b61119d1
Sho Neriki <ikirenohs@gmail.com>
2023-06-15 16:08:44 +0900

    added excerpts as an array of strings

--- server/src/models/PracticePlans.js
+++ server/src/models/PracticePlans.js
@@ -1,31 +1,36 @@
 const mongoose = require('mongoose');
 
-const practicePlanSchema = new mongoose.Schema({
-  pieceName: {
-    type: String,
+const practicePlanSchema = new mongoose.Schema(
+  {
+    pieceName: {
+      type: String,
+    },
+    composer: {
+      type: String,
+    },
+    excerpts: [
+      {
+      type: String,
+    }
+    ],
+    movements: {
+      type: String,
+    },
+    endMetronomeGoal: {
+      type: Number,
+    },
+    practiceDates: [{ type: Date }],
+    daily: { type: Boolean, default: false },
+    timesPerWeek: { type: Number, min: 1, max: 7 },
+    untilDate: { type: Date },
+    practiceLengthInMinutes: { type: Number, min: 1, max: 1440 },
+    userOwner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
-  composer: {
-    type: String,
-  },
-  excerpts: {
-    type: String,
-  },
-  movements: {
-    type: String,
-  },
-  endMetronomeGoal: {
-    type: Number,
-  },
-  practiceDates: [{type: Date}],
-  daily: {type: Boolean, default: false},
-  timesPerWeek: {type: Number, min: 1, max: 7},
-  untilDate: {type: Date},
-  practiceLengthInMinutes: {type: Number, min: 1, max: 1440},
-  userOwner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-}, {timestamps: true});
+  { timestamps: true }
+);
 
 export const practicePlanModel = mongoose.model("PracticePlans", practicePlanSchema)
