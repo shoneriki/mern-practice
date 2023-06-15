27c03fc4de9711db682f21df6f366226f6c0ed59
Sho Neriki <ikirenohs@gmail.com>
2023-06-15 10:44:55 +0900

    post request for recipe

--- server/src/index.js
+++ server/src/index.js
@@ -3,13 +3,14 @@ import cors from "cors";
 import mongoose from "mongoose";
 import "dotenv/config.js";
 import {userRouter} from './routes/users.js'
-
+import {recipesRouter} from './routes/recipes.js'
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use("/auth", userRouter)
+app.use("/recipes", recipesRouter)
 
 
 mongoose.connect(process.env.MONGO_URI, {
