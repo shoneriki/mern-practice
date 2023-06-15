6fdcd4eb133cafa5297387a692c5a4c150e3e89b
Sho Neriki <ikirenohs@gmail.com>
2023-06-15 12:36:32 +0900

    starting form for creating recipes

--- client/src/pages/create-recipe.js
+++ client/src/pages/create-recipe.js
@@ -1,3 +1,36 @@
 export const CreateRecipe = () => {
-  return <div>Create Recipe</div>;
+  return (
+    <div className="create-recipe">
+      <h2>Create Recipe</h2>
+      <form >
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+        />
+        <label htmlFor="description">Description</label>
+        <textarea
+          id="description"
+          name="description"
+        ></textarea>
+        <label htmlFor="ingredients">Ingredients</label>
+        <label htmlFor="instructions">Instructions</label>
+        <textarea
+          id="instructions"
+          name="instructions"
+        ></textarea>
+        <label htmlFor="imageUrl">Image URL</label>
+        <input
+          type="text"
+          id="imageUrl"
+        />
+        <label htmlFor="cookingTime">Cooking Time (minutes)</label>
+        <input
+          type="number"
+          id="cookingTime"
+          name="cookingTime"
+        />
+      </form>
+    </div>
+  );
 };
