d50a5121949c5eee3e511a4b429c38df9c29335c
Sho Neriki <ikirenohs@gmail.com>
2023-06-20 17:00:58 +0900

    started workspace

---
+++ client/src/pages/workspace.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+import { useGetUserID } from "../hooks/useGetUserID";
+import axios from "axios";
+
+import { useNavigate } from "react-router-dom";
+
+import { Button } from "@mui/material";
+
+export const Workspace = () => {
+  
+}
