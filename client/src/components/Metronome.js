1966a31f406854ca3cb7ca59642110a7280164bd
Sho Neriki <ikirenohs@gmail.com>
2023-06-21 13:38:03 +0900

    metronome technically working

---
+++ client/src/components/Metronome.js
@@ -0,0 +1,101 @@
+import React, { Component } from "react";
+import click from "../assets/sounds/click.mp3"
+import woodblock from "../assets/sounds/woodblock.mp3"
+
+import { Button, Slider, Box, Typography, Input } from "@mui/material";
+
+
+export class Metronome extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isPlaying: false,
+      count: 0,
+      bpm: 60,
+      beatsPerMeasure: 4,
+    };
+
+    this.click1 = new Audio(click);
+    this.click2 = new Audio(woodblock);
+  }
+
+  handleInputChange = (event, newValue) => {
+    const bpm = newValue;
+
+    if (this.state.isPlaying) {
+      // stop old timer and start a new one
+      clearInterval(this.timer);
+      this.timer = setInterval(this.playClick, (60 / bpm) * 1000);
+
+      // set the new bpm
+      // and reset the beat counter
+      this.setState({
+        count: 0,
+        bpm,
+      });
+    } else {
+      // otherwise, just update the bpm
+      this.setState({ bpm });
+    }
+  };
+
+  playClick = () => {
+    const { count, beatsPerMeasure } = this.state;
+
+    // alternate click sounds
+    if (count % beatsPerMeasure === 0) {
+      this.click2.play();
+    } else {
+      this.click1.play();
+    }
+
+    // keep track of which beat we're on
+    this.setState((state) => ({
+      count: (state.count + 1) % state.beatsPerMeasure,
+    }));
+  };
+
+  startStop = () => {
+    if (this.state.isPlaying) {
+      // stop the timer
+      clearInterval(this.timer);
+      this.setState({
+        isPlaying: false,
+      });
+    } else {
+      // start a timer with current bpm
+      this.timer = setInterval(this.playClick, (60 / this.state.bpm) * 1000);
+      this.setState(
+        {
+          count: 0,
+          isPlaying: true,
+          // play a click immediately (after setState finishes)
+        },
+        this.playClick
+      );
+    }
+  };
+
+  render() {
+    const { isPlaying, bpm } = this.state;
+
+    return (
+      <Box sx={{ width: "80%" }} className="metronome">
+        <Box className="bpm-slider">
+          <p>{bpm} BPM</p>
+          <Slider
+            type="range"
+            min={10}
+            max={300}
+            value={bpm}
+            onChange={this.handleInputChange}
+          />
+        </Box>
+        <Button onClick={this.startStop}>{isPlaying ? "Stop" : "Start"}</Button>
+      </Box>
+    );
+  }
+}
+
+export default Metronome;
--- client/src/pages/workspace.js
+++ client/src/pages/workspace.js
@@ -1,86 +1,14 @@
 import React, { useState, useRef } from "react";
-import { Button, Slider, Box, Typography } from "@mui/material";
+import {Metronome} from "../components/Metronome"
+import { Button, Slider, Box, Typography, Input } from "@mui/material";
+
 
 export const Workspace = () => {
-  const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-  const [bpm, setBpm] = useState(100);
-  const [playing, setPlaying] = useState(false);
-  const [count, setCount] = useState(0);
-  const [beatsPerMeasure, setBeatsPerMeasure] = useState(4);
-  const lookahead = 25.0;
-  const scheduleAheadTime = 0.1;
-  const nextNoteTime = useRef(audioContext.currentTime + 0.05);
-  const timerID = useRef();
-  const notesInQueue = useRef([]);
 
-  function nextNote() {
-    const secondsPerBeat = 60.0 / bpm;
-    nextNoteTime.current = nextNoteTime.current + secondsPerBeat;
-    setCount((prevCount) => (prevCount + 1) % beatsPerMeasure);
-  }
-
-  function scheduleNote(beatNumber, time) {
-    notesInQueue.current = notesInQueue.current.concat({
-      note: beatNumber,
-      time,
-    });
-    const osc = audioContext.createOscillator();
-    osc.connect(audioContext.destination);
-    if (beatNumber % beatsPerMeasure === 0) osc.frequency.value = 880.0;
-    else osc.frequency.value = 440.0;
-    osc.start(time);
-    osc.stop(time + 0.05);
-  }
-
-  function scheduler() {
-    while (
-      nextNoteTime.current <
-      audioContext.currentTime + scheduleAheadTime
-    ) {
-      scheduleNote(count, nextNoteTime.current);
-      nextNote();
-    }
-    timerID.current = window.setTimeout(scheduler, lookahead);
-  }
-
-  function startStop() {
-    setCount(0);
-    nextNoteTime.current = audioContext.currentTime + 0.05;
-    setPlaying((prevPlaying) => !prevPlaying);
-    if (!playing) {
-      scheduler();
-    } else {
-      window.clearTimeout(timerID.current);
-      notesInQueue.current = [];
-    }
-  }
-
-  function handleBpmChange(e, val) {
-    setBpm(val);
-    if(playing) {
-      window.clearTimeout(timerID.current);
-      notesInQueue.current = []
-      setCount(0);
-      nextNoteTime.current = audioContext.currentTime + 0.05;
-      scheduler();
-    }
-  }
 
   return (
-    <Box sx={{ width: 200 }}>
-      <Typography id="input-slider" gutterBottom>
-        Tempo (BPM) {bpm}
-      </Typography>
-      <Slider
-        value={bpm}
-        onChange={handleBpmChange}
-        aria-labelledby="input-slider"
-        valueLabelDisplay="auto"
-        step={1}
-        min={10}
-        max={200}
-      />
-      <Button onClick={startStop}>{playing ? "Stop" : "Start"}</Button>
-    </Box>
+    <>
+    <Metronome/>
+    </>
   );
 };
