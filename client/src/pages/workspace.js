930a47ec722f9c27df907f5aef328403db4520b7
Sho Neriki <ikirenohs@gmail.com>
2023-06-21 14:07:34 +0900

    changed to metronome with class

--- client/src/pages/workspace.js
+++ client/src/pages/workspace.js
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from "react";
 import {Metronome} from "../components/Metronome"
+import {MetronomeE6} from "../components/MetronomeE6"
 import { Button, Slider, Box, Typography, Input } from "@mui/material";
 
 
@@ -8,7 +9,7 @@ export const Workspace = () => {
 
   return (
     <>
-    <Metronome/>
+    <Metronome />
     </>
   );
 };
