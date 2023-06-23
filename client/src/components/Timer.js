efc8aa44e243f546541b96e6190ef0f238c79a83
Sho Neriki <ikirenohs@gmail.com>
2023-06-23 17:09:49 +0900

    started timer

---
+++ client/src/components/Timer.js
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+
+import { Box } from "@mui/material";
+
+export const Timer = () => {
+
+  const [day, setDays] = useState(0);
+  const [hours, setHours] = useState(0)
+  return (
+    <Box >
+
+    </Box>
+  )
+}
