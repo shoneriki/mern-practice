3de1d630e0e9d9215538dd29d3eb769aa17aee8f
Sho Neriki <ikirenohs@gmail.com>
2023-06-25 15:43:24 +0900

    calendar functionality fixed

--- client/src/services/useFetchProgramsForCalendar.js
+++ client/src/services/useFetchProgramsForCalendar.js
@@ -8,52 +8,66 @@ export function useFetchProgramsForCalendar() {
 
   useEffect(() => {
     const fetchProgramsForCalendar = async () => {
-      const res = await axios.get(`http://localhost:3001/programs/:${userID}`);
-      const programs = res.data;
-      console.log("res.data", res.data)
-      if (programs && programs.length > 0) {
-        const updatedPrograms = programs.map((program) => {
-              let currentStartTime = new Date(program.date);
-              let currentEndTime = new Date(program.date);
-              const events = [];
+      try {
+        const res = await axios.get(`http://localhost:3001/programs/${userID}`);
+        const programs = res.data;
 
+        console.log("HTTP status: ", res.status);
+        console.log("Programs data: ", programs);
+
+        if (programs && programs.length > 0) {
+          const updatedPrograms = programs.map((program) => {
+            let currentStartTime = new Date(program.date);
+            let currentEndTime = new Date(program.date);
+            const events = [];
+
+            if (program.pieces) {
               for (let i = 0; i < program.pieces.length; i++) {
-                const pieceLengthInSeconds =
-                  program.pieces[i].length.hours * 3600 +
-                  program.pieces[i].length.minutes * 60 +
-                  program.pieces[i].length.seconds;
+                if (program.pieces[i].length) {
+                  const pieceLengthInSeconds =
+                    program.pieces[i].length.hours * 3600 +
+                    program.pieces[i].length.minutes * 60 +
+                    program.pieces[i].length.seconds;
 
-                currentEndTime = new Date(
-                  currentEndTime.getTime() + pieceLengthInSeconds * 1000
-                );
+                  currentEndTime = new Date(
+                    currentEndTime.getTime() + pieceLengthInSeconds * 1000
+                  );
 
-                events.push({
-                  start: new Date(currentStartTime),
-                  end: new Date(currentEndTime),
-                  title: `${program.pieces[i].name} by ${program.pieces[i].composer}`,
-                });
+                  events.push({
+                    start: new Date(currentStartTime),
+                    end: new Date(currentEndTime),
+                    title: `${program.pieces[i].name} by ${program.pieces[i].composer}`,
+                  });
 
-                currentStartTime = new Date(currentEndTime.getTime());
+                  currentStartTime = new Date(currentEndTime.getTime());
+                }
               }
+            }
 
-              currentEndTime = new Date(
-                currentEndTime.getTime() + program.intermission * 60 * 1000
-              );
-              events.push({
-                start: new Date(currentStartTime),
-                end: new Date(currentEndTime),
-                title: "Intermission",
-              });
+            currentEndTime = new Date(
+              currentEndTime.getTime() + program.intermission * 60 * 1000
+            );
 
-        });
-        setPrograms(updatedPrograms);
-      } else {
-        setPrograms([]);
+            events.push({
+              start: new Date(currentStartTime),
+              end: new Date(currentEndTime),
+              title: "Intermission",
+            });
+
+            return { ...program, events };
+          });
+
+          setPrograms(updatedPrograms);
+        } else {
+          setPrograms([]);
+        }
+      } catch (error) {
+        console.error("Error fetching programs: ", error);
       }
     };
 
     fetchProgramsForCalendar();
-  }, [userID]); // userID is a dependency, so we add it to dependency array
+  }, [userID]);
 
   return programs;
 }
