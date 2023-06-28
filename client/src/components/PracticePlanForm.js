78e8a61e04f9fe507544cb7c8ecaa3cd68377112
Sho Neriki <ikirenohs@gmail.com>
2023-06-28 12:02:17 +0900

    frontend form working, including daily/non-daily functionality

--- client/src/components/PracticePlanForm.js
+++ client/src/components/PracticePlanForm.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import {Box} from "@mui/material"
+import {Box, Button, Grid, TextField, Radio, RadioGroup, FormControlLabel} from "@mui/material"
 
 export const PracticePlanForm = ({
   practicePlan,
@@ -12,120 +12,160 @@ export const PracticePlanForm = ({
   return (
     <form className="practice-plan-form" onSubmit={handleSubmit}>
       <Box
+        name="Form Box"
         sx={{
           display: "flex",
           flexDirection: "column",
         }}
       >
-        <label htmlFor="pieceTitle">Piece Title:</label>
-        <input
-          type="text"
-          id="pieceTitle"
-          name="pieceTitle"
-          value={practicePlan.pieceTitle}
-          onChange={handleChange}
-        />
-        {suggestions.length > 0 && (
-          <section
-            style={{
-              border: "1px solid #ccc",
-              borderRadius: "4px",
-              marginTop: "2px",
-              maxHeight: "150px",
-              overflowY: "auto",
-            }}
-          >
-            {suggestions.map((suggestion) => (
-              <section
-                key={suggestion._id}
-                style={{ padding: "4px", cursor: "pointer" }}
-                onClick={() => handleValueChange(suggestion)}
+        <Grid container spacing={2} name="form-grid">
+          <Grid item xs={12}>
+            <TextField
+              id="pieceTitle"
+              name="pieceTitle"
+              label="Piece Title"
+              value={practicePlan.pieceTitle}
+              onChange={handleChange}
+              fullWidth
+            />
+            {suggestions.length > 0 && (
+              <Box
+                name="suggestion-box"
+                sx={{
+                  border: "1px solid #ccc",
+                  borderRadius: "4px",
+                  marginTop: "2px",
+                  maxHeight: "150px",
+                  overflowY: "auto",
+                }}
               >
-                {suggestion.name} by {suggestion.composer},
-              </section>
-            ))}
-          </section>
-        )}
-      </Box>
-      <label htmlFor="Composer">Composer:</label>
-      <input
-        type="text"
-        id="composer"
-        name="composer"
-        value={practicePlan.composer}
-        onChange={handleChange}
-      />
+                {suggestions.map((suggestion) => (
+                  <Box
+                    name="suggestion-item"
+                    key={suggestion._id}
+                    sx={{ padding: "4px", cursor: "pointer" }}
+                    onClick={() => handleValueChange(suggestion)}
+                  >
+                    {suggestion.name} by {suggestion.composer},
+                  </Box>
+                ))}
+              </Box>
+            )}
+          </Grid>
+          <Grid name="composer-grid" item xs={12}>
+            <TextField
+              id="composer"
+              name="composer"
+              label="Composer"
+              value={practicePlan.composer}
+              onChange={handleChange}
+              fullWidth
+            />
+          </Grid>
+          <Grid name="practiceStartDate-grid" item xs={12}>
+            <TextField
+              id="practiceStartDate"
+              name="practiceStartDate"
+              label="Date of Practice Start"
+              type="date"
+              value={practicePlan.practiceStartDate}
+              onChange={handleChange}
+              fullWidth
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </Grid>
+          <Grid name="daily-grid" item xs={12}>
+            <RadioGroup
+              row
+              name="daily"
+              value={practicePlan.daily}
+              onChange={(event) => {
+                handleChange({
+                  target: {
+                    name: event.target.name,
+                    value: event.target.value === "true",
+                  },
+                });
+              }}
+            >
+              <FormControlLabel
+                value={true}
+                control={<Radio />}
+                label="Daily"
+              />
+              <FormControlLabel
+                value={false}
+                control={<Radio />}
+                label="Not Daily"
+              />
+            </RadioGroup>
+          </Grid>
 
-      <label htmlFor="Practice Start Date">Date of Practice Start:</label>
-      <input
-        type="date"
-        id="practiceStartDate"
-        name="practiceStartDate"
-        value={practicePlan.practiceStartDate}
-        onChange={handleChange}
-      />
-      <section>
-        <label htmlFor="daily">Daily?</label>
-        <input
-          type="radio"
-          id="daily-yes"
-          name="daily"
-          value={true}
-          checked={practicePlan.daily === true}
-          onChange={handleChange}
-        />
-        Yes
-        <input
-          type="radio"
-          id="daily-no"
-          name="daily"
-          value={false}
-          checked={practicePlan.daily === false}
-          onChange={handleChange}
-        />
-        No
-      </section>
-      {!practicePlan.daily ? (
-        <>
-          <label htmlFor="timesPerWeek">Times Per Week</label>
-          <input
-            type="number"
-            id="timesPerWeek"
-            name="timesPerWeek"
-            min="1"
-            max="7"
-            value={practicePlan.timesPerWeek}
-            onChange={handleChange}
-          />
-        </>
-      ) : null}
-      <label htmlFor="untilDate">Until Date:</label>
-      <input
-        type="date"
-        id="untilDate"
-        name="untilDate"
-        value={practicePlan.untilDate}
-        onChange={handleChange}
-      />
-      <label htmlFor="practiceLengthInMinutes">
-        Length of practice session(minutes)
-      </label>
-      <input
-        type="number"
-        id="practiceLengthInMinutes"
-        name="practiceLengthInMinutes"
-        value={practicePlan.practiceLengthInMinutes}
-        onChange={handleChange}
-      />
-      <label htmlFor="notes">Extra notes?:</label>
-      <textarea
-        type="date"
-        id="notes"
-        name="notes"
-        value={practicePlan.notes}
-        onChange={handleChange}
-      />
-      <button type="submit">Create Plan</button>
+          {!practicePlan.daily && (
+            <Grid name="timesPerWeek-grid" item xs={12}>
+              <TextField
+                id="timesPerWeek"
+                name="timesPerWeek"
+                label="Times Per Week"
+                type="number"
+                InputProps={{ inputProps: { min: 1, max: 7 } }}
+                value={practicePlan.timesPerWeek}
+                onChange={handleChange}
+                fullWidth
+              />
+            </Grid>
+          )}
+
+          <Grid name="untilDate-grid" item xs={12}>
+            <TextField
+              id="untilDate"
+              name="untilDate"
+              label="Until Date"
+              type="date"
+              value={practicePlan.untilDate}
+              onChange={handleChange}
+              fullWidth
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </Grid>
+
+          <Grid name="practiceLengthInMinutes-grid" item xs={12}>
+            <TextField
+              id="practiceLengthInMinutes"
+              name="practiceLengthInMinutes"
+              label="Length of practice session(minutes)"
+              type="number"
+              InputProps={{ inputProps: { min: 1, max: 1440 } }}
+              value={practicePlan.practiceLengthInMinutes}
+              onChange={handleChange}
+              fullWidth
+            />
+          </Grid>
+
+          <Grid name="notes-grid" item xs={12}>
+            <TextField
+              id="notes"
+              name="notes"
+              label="Extra notes"
+              multiline
+              rows={4}
+              value={practicePlan.notes}
+              onChange={handleChange}
+              fullWidth
+            />
+          </Grid>
+
+          <Grid name="create-plan-btn-grid" item xs={12}>
+            <Button variant="contained" color="primary" type="submit">
+              Create Plan
+            </Button>
+          </Grid>
+        </Grid>
+      </Box>
     </form>
   );
 };
