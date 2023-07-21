0a55cadec9c040e08afa9ea17b338796d37d6f38
Sho Neriki <ikirenohs@gmail.com>
2023-07-21 12:00:19 +0900

    changed layout for the buttons

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -8,6 +8,7 @@ import { ScheduledCalendar } from "../components/Calendar";
 import {
   Button,
   Box,
+  Grid,
 } from "@mui/material";
 
 import {ProgramList} from "../components/program-components/ProgramList"
@@ -16,45 +17,49 @@ export const Home = () => {
 
   const userID = useGetUserID();
 
-  const [_, setPrograms] = useState([]);
-
-    useEffect(() => {
-      const fetchPrograms = async () => {
-        try {
-          const response = await axios.get(`http://localhost:3001/programs/user/${userID}`);
-
-          response.data && response.data.length > 0 ? setPrograms(response.data) : setPrograms([])
-        } catch (err) {
-          console.log(err);
-        }
-      }
-      fetchPrograms()
-  }, [userID]);
 
   const navigate = useNavigate();
 
   return (
-    <Box className="container"
-      sx={{display: "flex", flexDirection: "column", alignItems: "center", width: "100%"}}
+    <Box
+      className="container"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        width: "100%",
+      }}
     >
       <ProgramList />
-      <Box sx={{display: "flex", justifyContent: "space-between", alignItems: "center", margin: "1rem auto", width: "40%"}}>
-        <Button
-          onClick={() => navigate("/programs")}
-          color='secondary'
-          variant="contained"
-        >
-          All Programs?
-        </Button>
-        <Button
-          onClick={() => navigate("/practiceSessions")}
-          color='secondary'
-          variant="contained"
-        >
-          All Practice Sessions?
-        </Button>
-      </Box>
-      <Box id="calendar">
+      <Grid
+        container
+        spacing={4}
+        justifyContent="center"
+        sx={{
+          margin: "1rem auto",
+          width: "80%",
+        }}
+      >
+        <Grid item xs={12} sx={{ display: "flex", justifyContent: "center" }}>
+          <Button
+            onClick={() => navigate("/programs")}
+            color="secondary"
+            variant="contained"
+          >
+            All Programs?
+          </Button>
+        </Grid>
+        <Grid item xs={12} sx={{ display: "flex", justifyContent: "center" }}>
+          <Button
+            onClick={() => navigate("/practiceSessions")}
+            color="secondary"
+            variant="contained"
+          >
+            All Practice Sessions?
+          </Button>
+        </Grid>
+      </Grid>
+      <Box id="calendar" sx={{ width: "80%", margin: "2rem auto" }}>
         <ScheduledCalendar />
       </Box>
     </Box>
