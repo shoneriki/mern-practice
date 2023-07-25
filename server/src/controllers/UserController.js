86d17af9bee8139213d4b90854cd2205768823ff
Sho Neriki <ikirenohs@gmail.com>
2023-07-25 13:14:17 +0900

    added console log to server-side

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -4,6 +4,8 @@ import { UserModel } from "../models/Users.js";
 
 const register = async (req, res) => {
   const { username, password } = req.body;
+
+  console.log("inside the register request...")
   const user = await UserModel.findOne({ username });
 
   if (user) {
@@ -13,7 +15,9 @@ const register = async (req, res) => {
   const hashedPassword = await bcrypt.hash(password, 10);
 
   const newUser = new UserModel({ username, password: hashedPassword });
+  console.log("new User?", newUser)
   await newUser.save();
+  console.log("past save")
 
   const token = jwt.sign({ id: newUser._id }, process.env.SECRET);
 
@@ -22,11 +26,14 @@ const register = async (req, res) => {
     token,
     userID: newUser._id,
   });
+  console.log("end of register")
 };
 
 const login = async (req, res) => {
   const { username, password } = req.body;
   const user = await UserModel.findOne({ username });
+
+  console.log("inside login... user: ", user)
 
   if (!user) {
     return res.json({ status: 'error', message: "User Doesn't Exist..." });
@@ -38,8 +45,10 @@ const login = async (req, res) => {
     return res.json({ message: "username or password is incorrect" });
   }
 
+
   const token = jwt.sign({ id: user._id }, process.env.SECRET);
   res.json({ token, userID: user._id });
+  console.log("end of login")
 };
 
 // const verifyToken = (req, res, next) => {
