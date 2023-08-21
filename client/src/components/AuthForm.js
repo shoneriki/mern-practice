d0c259b22adb7638cd0f1d1ccce7a4706a4f69aa
Sho Neriki <ikirenohs@gmail.com>
2023-08-21 14:27:21 +0900

    changed form style

--- client/src/components/AuthForm.js
+++ client/src/components/AuthForm.js
@@ -3,8 +3,16 @@ import {Button, Box, Typography} from "@mui/material"
 export const AuthForm = ({username, setUsername, password, setPassword, handleSubmit, label, logIn, setLogIn, loggedIn, setLoggedIn}) => {
   return (
     <Box>
-      <form onSubmit={handleSubmit}>
-        <Typography variant={'h6'} sx={{margin: "1rem 0"}}>{label}</Typography>
+      <form
+        onSubmit={handleSubmit}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItem: "center",
+        }}
+      >
+        <Typography variant={'h6'} sx={{margin: "1rem 0", textAlign: "center"}}>{label}</Typography>
         <div className="form-group">
           <label htmlFor="username">Username:</label>
           <input
@@ -28,7 +36,8 @@ export const AuthForm = ({username, setUsername, password, setPassword, handleSu
           color="primary"
           type="submit"
           sx={{
-            margin: "1rem 0"
+            margin: "1rem 0",
+            width: "100%",
           }}
         >
           {label}
