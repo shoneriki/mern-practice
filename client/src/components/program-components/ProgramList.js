874cb684700a19393e316e6fb471d68997a2e13a
Sho Neriki <ikirenohs@gmail.com>
2023-08-24 13:41:55 +0900

    add program button added for program list

--- client/src/components/program-components/ProgramList.js
+++ client/src/components/program-components/ProgramList.js
@@ -92,28 +92,30 @@ export const ProgramList = () => {
   // delete functionality
 
   const [open, setOpen] = useState(false);
-  const [_, setToDelete] = useState(null);
+  const [programToDelete, setProgramToDelete] = useState(null);
 
   const handleClickOpen = (id) => {
     setOpen(true);
-    console.log(`is id 64e586e834184ed1956ba9ed? id is ${id}`);
-    setToDelete(id);
+    console.log(
+      `the id should be this id 64e6d8606f4ce4412d41fcf1, the actual id is ${id}`
+    );
+    setProgramToDelete(id);
   };
 
   const handleClose = () => {
     setOpen(false);
   };
 
-  const handleDelete = async (id) => {
+  const handleDelete = async () => {
     try {
       await axios.delete(
-        `${process.env.REACT_APP_API_URL}/programs/program/${id}`
+        `${process.env.REACT_APP_API_URL}/programs/program/${programToDelete}`
         );
       console.log(
-        `program with intended id is 64e586e834184ed1956ba9ed. The program with id ${id} is deleted`
+        `the id should be 64e6d8606f4ce4412d41fcf1, the deleted program's id was ${programToDelete}`
       );
       setOpen(false);
-      setPrograms(programs.filter((program) => program._id !== id));
+      setPrograms(programs.filter((program) => program._id !== programToDelete));
       console.log("programs now? ", programs)
     } catch (err) {
       console.log("error: ", err);
@@ -127,7 +129,7 @@ export const ProgramList = () => {
       {loading ? (
         <section>Loading...</section>
       ) : (
-        <Box className="programList" sx={{ width: "80%", margin: "2rem auto",  }}>
+        <Box className="programList" sx={{ width: "80%", margin: "2rem auto" }}>
           <Typography
             variant={"h4"}
             sx={{ textAlign: "center", margin: "1rem auto", width: "80%" }}
@@ -136,138 +138,161 @@ export const ProgramList = () => {
           </Typography>
           <Grid container spacing={3}>
             {Array.isArray(programs) && programs.length !== 0 ? (
-              programs.map((program, programIndex) => {
-                return (
-                  <Grid item centered xs={12} sm={6} md={4} key={program._id}>
-                    <Card
-                      sx={{
-                        border: "1px solid black",
-                        borderRadius: "1rem",
-                        padding: "1rem",
-                        width: "80%",
-                      }}
-                    >
-                      <CardContent>
-                        <Typography variant={"h6"} sx={{ fontWeight: "bold" }}>
-                          Program No. {programIndex + 1}:
-                        </Typography>
-                        <Typography variant={"h6"}>{program.name}</Typography>
-                        <Typography variant={"h6"} sx={{ fontWeight: "bold" }}>
-                          Date:
-                        </Typography>
-                        <Typography variant={"h6"}>
-                          {format(
-                            new Date(program.date),
-                            "MMMM do, yyyy 'at' H:mm"
-                          )}
-                        </Typography>
-                        <Box>
-                          {program.pieces.map((piece, index) => {
-                            const {
-                              hours: pieceHours,
-                              minutes: pieceMinutes,
-                              seconds: pieceSeconds,
-                            } = piece.length;
-                            return (
-                              <Box className="piece-display" key={piece._id}>
-                                <Typography sx={{ fontWeight: "bold" }}>
-                                  Piece {index + 1}:
-                                </Typography>
-                                <Typography>{piece.name}</Typography>
-                                <Typography sx={{ fontWeight: "bold" }}>
-                                  Composer:
-                                </Typography>
-                                <Typography>{piece.composer}</Typography>
-                                <Typography sx={{ fontWeight: "bold" }}>
-                                  Length Of Piece:
-                                </Typography>
-                                <Typography>
-                                  {pieceHours}hr: {pieceMinutes}min:{" "}
-                                  {pieceSeconds}
-                                  sec
-                                </Typography>
-                              </Box>
-                            );
-                          })}
-                        </Box>
-                        <Typography>
-                          Intermission: {program.intermission} minutes
-                        </Typography>
-                        <Typography sx={{ fontWeight: "bold" }}>
-                          Program Length:
-                        </Typography>
-                        <Typography>
-                          {program.length.hours}hr: {program.length.minutes}
-                          min: {program.length.seconds}sec
-                        </Typography>
-                      </CardContent>
-
-                      <CardActions
-                        name="delete-edit-btn-box"
+              <>
+                {programs.map((program, programIndex) => {
+                  return (
+                    <Grid item centered xs={12} sm={6} md={4} key={program._id}>
+                      <Card
                         sx={{
-                          display: "flex",
-                          justifyContent: "space-between",
-                          alignItems: "center",
+                          border: "1px solid black",
+                          borderRadius: "1rem",
+                          padding: "1rem",
+                          width: "80%",
                         }}
                       >
-                        <Button
-                          name="edit-btn"
-                          variant="outlined"
+                        <CardContent>
+                          <Typography
+                            variant={"h6"}
+                            sx={{ fontWeight: "bold" }}
+                          >
+                            Program No. {programIndex + 1}:
+                          </Typography>
+                          <Typography variant={"h6"}>{program.name}</Typography>
+                          <Typography
+                            variant={"h6"}
+                            sx={{ fontWeight: "bold" }}
+                          >
+                            Date:
+                          </Typography>
+                          <Typography variant={"h6"}>
+                            {format(
+                              new Date(program.date),
+                              "MMMM do, yyyy 'at' H:mm"
+                            )}
+                          </Typography>
+                          <Box>
+                            {program.pieces.map((piece, index) => {
+                              const {
+                                hours: pieceHours,
+                                minutes: pieceMinutes,
+                                seconds: pieceSeconds,
+                              } = piece.length;
+                              return (
+                                <Box className="piece-display" key={piece._id}>
+                                  <Typography sx={{ fontWeight: "bold" }}>
+                                    Piece {index + 1}:
+                                  </Typography>
+                                  <Typography>{piece.name}</Typography>
+                                  <Typography sx={{ fontWeight: "bold" }}>
+                                    Composer:
+                                  </Typography>
+                                  <Typography>{piece.composer}</Typography>
+                                  <Typography sx={{ fontWeight: "bold" }}>
+                                    Length Of Piece:
+                                  </Typography>
+                                  <Typography>
+                                    {pieceHours}hr: {pieceMinutes}min:{" "}
+                                    {pieceSeconds}
+                                    sec
+                                  </Typography>
+                                </Box>
+                              );
+                            })}
+                          </Box>
+                          <Typography>
+                            Intermission: {program.intermission} minutes
+                          </Typography>
+                          <Typography sx={{ fontWeight: "bold" }}>
+                            Program Length:
+                          </Typography>
+                          <Typography>
+                            {program.length.hours}hr: {program.length.minutes}
+                            min: {program.length.seconds}sec
+                          </Typography>
+                        </CardContent>
+
+                        <CardActions
+                          name="delete-edit-btn-box"
                           sx={{
-                            backgroundColor: "yellow",
-                            color: "black",
-                            border: "none",
-                            "&:hover": {
-                              backgroundColor: "orange",
-                              border: "none",
-                              cursor: "pointer",
-                            },
+                            display: "flex",
+                            justifyContent: "space-between",
+                            alignItems: "center",
                           }}
-                          onClick={() => handleEdit(program._id)}
                         >
-                          Edit?
-                        </Button>
-                        <Button
-                          name="delete-btn"
-                          variant="outlined"
-                          sx={{
-                            backgroundColor: "red",
-                            color: "white",
-                            border: "none",
-                            "&:hover": {
-                              backgroundColor: "darkred",
+                          <Button
+                            name="edit-btn"
+                            variant="outlined"
+                            sx={{
+                              backgroundColor: "yellow",
+                              color: "black",
                               border: "none",
-                              cursor: "pointer",
-                            },
-                          }}
-                          onClick={() => handleClickOpen(program._id)}
-                        >
-                          Delete
-                        </Button>
-                        <Dialog name="dialog" open={open} onClose={handleClose}>
-                          <DialogTitle>
-                            {"Are you sure you want to delete this program?"}
-                          </DialogTitle>
-                          <DialogActions>
-                            <Button onClick={handleClose} color="primary">
-                              Cancel
-                            </Button>
-                            <Button
-                              onClick={() => handleDelete(program._id)}
-                              color="primary"
-                              autoFocus
-                            >
-                              Yes, Delete
-                            </Button>
-                          </DialogActions>
-                        </Dialog>
-                      </CardActions>
-                    </Card>
-                  </Grid>
-                );
-              })
+                              "&:hover": {
+                                backgroundColor: "orange",
+                                border: "none",
+                                cursor: "pointer",
+                              },
+                            }}
+                            onClick={() => handleEdit(program._id)}
+                          >
+                            Edit?
+                          </Button>
+                          <Button
+                            name="delete-btn"
+                            variant="outlined"
+                            sx={{
+                              backgroundColor: "red",
+                              color: "white",
+                              border: "none",
+                              "&:hover": {
+                                backgroundColor: "darkred",
+                                border: "none",
+                                cursor: "pointer",
+                              },
+                            }}
+                            onClick={() => handleClickOpen(program._id)}
+                          >
+                            Delete
+                          </Button>
+                          <Dialog
+                            name="dialog"
+                            open={open}
+                            onClose={handleClose}
+                          >
+                            <DialogTitle>
+                              {"Are you sure you want to delete this program?"}
+                            </DialogTitle>
+                            <DialogActions>
+                              <Button onClick={handleClose} color="primary">
+                                Cancel
+                              </Button>
+                              <Button
+                                onClick={() => handleDelete()}
+                                color="primary"
+                                autoFocus
+                              >
+                                Yes, Delete
+                              </Button>
+                            </DialogActions>
+                          </Dialog>
+                        </CardActions>
+                      </Card>
+                    </Grid>
+                  );
+                })}
+                <Grid item xs={12} style={{ textAlign: "center" }}>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => navigate(`/program/create`)}
+                  >
+                    Add Program?
+                  </Button>
+                </Grid>
+              </>
             ) : (
-              <Box sx={{ textAlign: "center", marginTop: "2rem", width: "100%" }}>
+              <Box
+                sx={{ textAlign: "center", marginTop: "2rem", width: "100%" }}
+              >
                 <Typography variant="h6">
                   There are no programs yet. Would you like to add one?
                 </Typography>
@@ -275,7 +300,7 @@ export const ProgramList = () => {
                   sx={{
                     display: "flex",
                     justifyContent: "center",
-                    width: "100%"
+                    width: "100%",
                   }}
                 >
                   <Button
